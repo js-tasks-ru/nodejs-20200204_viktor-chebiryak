36d06947fe6f7571cc2ea48a5f6e656d93cdcd29
Viktor Chebiriak <webvictor86@gmail.com>
2020-04-13 23:30:19 +0200

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,29 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      console.log(filepath);
+      if (!pathname || pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        res.end('Bad request');
+      }
 
+      fs.exists(filepath, function (exists) {
+	      console.log(exists);
+        if (!exists) {
+          res.statusCode = 404;
+          res.end('File not found');
+        }
+
+        fs.readFile(filepath, function (err, info) {
+          if (err) {
+            res.statusCode = 500;
+            res.end('Unknown server error');
+          }
+
+          res.statusCode = 200;
+          res.end(info);
+        })
+      });
       break;
 
     default:
